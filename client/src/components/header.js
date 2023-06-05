a184460de7c42c32d3308e754e709c3e067f7f2d
Chris Wilson <chrisw@myself.com>
2023-06-05 19:53:28 -0400

    components added, additional files and folders, pages and homepage done-ish

---
+++ client/src/components/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header class="Ask-Alice">
+      <h1 class=" purple font-display-bold">Ask Alice!</h1>
+    </header>
+  );
+};
+
+export default Header;
