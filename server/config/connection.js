cd1e58d45367f4af39338aa1d7eea8c8ead1c2cc
Norboro <ajbrook19@gmail.com>
2023-06-07 21:32:06 -0400

    Setup dotenv to fix mongodb connection errors.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI  || 'mongodb://localhost:27017/rabbit-hole', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/rabbit-hole', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
