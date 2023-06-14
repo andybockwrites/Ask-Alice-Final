a2e674278e4ac6461e93a08eaf21d22d852df5ae
Norboro <ajbrook19@gmail.com>
2023-06-14 17:15:15 -0400

    Edit error page

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-function Error({ currentPage, handlePageChange}) {
+function Error({ currentPage, handlePageChange }) {
     return (
-        <div className="body-container">
-            <h1>404: Page not found</h1>
+        <div className="body-container uk-flex uk-flex-column uk-flex-middle uk-margin-large-top">
+            <h1 className='uk-margin-small-bottom'>404: Page not found</h1>
+            <blockquote>"I don’t see how he can ever finish, if he doesn’t begin." - Alice</blockquote>
         </div>
     )
 }
