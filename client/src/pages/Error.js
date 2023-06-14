cfc8decc9cdfee9128a123e767dc3e65e9cb1c47
Norboro <ajbrook19@gmail.com>
2023-06-14 17:04:24 -0400

    Fix searchResults responsiveness further.
    Add random backgrounds when going through rabbit hole and make such that background color and image choice do not repeat twice in a row.

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -3,16 +3,7 @@ import React from 'react';
 function Error({ currentPage, handlePageChange}) {
     return (
         <div className="body-container">
-            <div className="sorry">
-                <h4 className="sorry-text brown font-display-bold">Sorry, there are no drug recalls within that range of dates.</h4>
-            </div>
-            <div className="quote">
-                <h6  className="brown font-display-bold">"I don’t see how he can ever finish, if he doesn’t begin." - Alice</h6>
-            </div>
-            <div className ="nav-container">
-                <button className="uk-link back-button"><a href="./index.html">Back to Search Dates</a></button>
-                <button id ="continuebtn" className="uk-link">Continue Down the Rabbit Hole!</button>
-            </div>
+            <h1>404: Page not found</h1>
         </div>
     )
 }
---
+++ client/src/utils/colors.js
@@ -0,0 +1,3 @@
+const colors = ['bg-turquoise', 'bg-green',  'bg-purple', 'bg-pink',  'bg-yellow'];
+
+export default colors;
